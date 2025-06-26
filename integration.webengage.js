69d0042d3184d3eea58bea7fe98d51c38c496801
Ananthu <ananthuap48@gmail.com>
2025-06-26 13:19:12 +0530

    def: disabled simulated version and enabled actual airtel submission

--- integration.webengage.js
+++ integration.webengage.js
@@ -122,34 +122,35 @@ exports.handleWebengage = async function ({ under, id, data }, dbConnection) {
         console.log("chat message converted!");
         
         
-        // let messageRequestId, error;
-        // try {
-        //     const response = await axios.post(process.env.SEND_MESSAGE_API, apiMessage, {
-        //         headers: {
-        //             "Content-Type": "application/json",
-        //             Authorization: `Basic ${Buffer.from(`${process.env.API_USERNAME}:${process.env.API_PASSWORD}`).toString("base64")}`
-        //         },
-        //         timeout: 5000
-        //     });
-        //     messageRequestId = response.data?.messageRequestId;
-        // } catch (err) {
-        //     error = {
-        //         title: err?.response?.data?.message || err?.message,
-        //         code: err?.response?.data?.code || null,
-        //         timestamp,
-        //         messageId
-        //     };
-        // }
         let messageRequestId, error;
         try {
-            const response = await simulateMessageSend(apiMessage)
+            const response = await axios.post(process.env.SEND_MESSAGE_API, apiMessage, {
+                headers: {
+                    "Content-Type": "application/json",
+                    Authorization: `Basic ${Buffer.from(`${process.env.API_USERNAME}:${process.env.API_PASSWORD}`).toString("base64")}`
+                },
+                // timeout: 5000
+            });
             messageRequestId = response.data?.messageRequestId;
         } catch (err) {
             error = {
-                title: err?.response?.data?.message || err?.message || "",
+                title: err?.response?.data?.message || err?.message || "Message undelivered!",
                 code: err?.response?.data?.code || null,
+                timestamp,
+                messageId
             };
         }
+
+        // let messageRequestId, error;
+        // try {
+        //     const response = await simulateMessageSend(apiMessage)
+        //     messageRequestId = response.data?.messageRequestId;
+        // } catch (err) {
+        //     error = {
+        //         title: err?.response?.data?.message || err?.message || "",
+        //         code: err?.response?.data?.code || null,
+        //     };
+        // }
         console.log("Airtel api processed sucessfully, error:", error);
         
         return {
